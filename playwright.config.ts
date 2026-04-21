bf65cc554e9552b646d0418c9872a0cb2413333f
JulijaMoskow <julijamoskovkina@gmail.com>
2026-04-21 11:14:57 +0300

    Update README for PR

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,24 +5,24 @@ import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
-testDir: './tests',
-fullyParallel: true,
-forbidOnly: !!process.env.CI,
-retries: process.env.CI ? 2 : 0,
-workers: process.env.CI ? 1 : undefined,
-reporter: 'html',
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
 
-use: {
-baseURL: process.env.APP_URL,
-trace: 'on-first-retry',
-},
+  use: {
+    baseURL: process.env.APP_URL,
+    trace: 'on-first-retry',
+  },
 
-projects: [
-{
-name: 'chromium',
-use: { ...devices['Desktop Chrome'] },
-},
-],
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 });
    // {
     //  name: 'firefox',
