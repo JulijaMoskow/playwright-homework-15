20ee39653fb65eb643324b022af02ab63b8cd850
JulijaMoskow <julijamoskovkina@gmail.com>
2026-04-21 11:05:29 +0300

    Complete homework 15

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,36 +4,26 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-console.log('APP_URL:', process.env.APP_URL);
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    baseURL: 'https://fe-delivery.tallinn-learning.ee/',
-    trace: 'on-first-retry',
-  },
+testDir: './tests',
+fullyParallel: true,
+forbidOnly: !!process.env.CI,
+retries: process.env.CI ? 2 : 0,
+workers: process.env.CI ? 1 : undefined,
+reporter: 'html',
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+use: {
+baseURL: process.env.APP_URL,
+trace: 'on-first-retry',
+},
 
+projects: [
+{
+name: 'chromium',
+use: { ...devices['Desktop Chrome'] },
+},
+],
+});
    // {
     //  name: 'firefox',
     //  use: { ...devices['Desktop Firefox'] },
--- tests/auth-negative.spec.ts
+++ tests/auth-negative.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test.describe('Auth negative tests', () => {
     test.beforeEach(async ({ page }) => {
@@ -16,7 +17,9 @@ test.describe('Auth negative tests', () => {
         const loginInput = page.getByPlaceholder('Login');
         const signInButton = page.getByRole('button', { name: 'Sign in' });
 
-        await loginInput.fill('testuser');
+        const randomUsername = faker.internet.username();
+
+        await loginInput.fill(randomUsername);
 
         await expect(signInButton).toBeDisabled();
     });
@@ -26,8 +29,11 @@ test.describe('Auth negative tests', () => {
         const passwordInput = page.getByPlaceholder('Password');
         const signInButton = page.getByRole('button', { name: 'Sign in' });
 
-        await loginInput.fill('testuser');
-        await passwordInput.fill('123');
+        const randomUsername = faker.internet.username();
+        const randomPassword = faker.internet.password({ length: 3 });
+
+        await loginInput.fill(randomUsername);
+        await passwordInput.fill(randomPassword);
 
         await expect(signInButton).toBeDisabled();
     });
